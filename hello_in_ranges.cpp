d5bfc6429ddef3fff7a0e0d64fd4130e91a10aaf
Samer William Al-Haddadin <samer.w.alhaddadin@gmail.com>
2020-10-27 20:41:25 +0300

    Ranges!! So Great!!!

--- hello_in_ranges.cpp
+++ hello_in_ranges.cpp
@@ -4,52 +4,24 @@
 #include <algorithm>
 #include <numeric>
 
+auto endline = []() { std::cout << '\n'; };
+
+
 int main()
 {
-    std::vector names { 
+    std::vector<std::string> names { 
         "Majeda", "Samer", "Shatha", "Adham", "Sahem", 
         "Fayek", "Lamar", "Essa", "Miral", 
         "Natasha", "William"
     };   
-    
-    // for (auto name : names) std::cout << name << ' ';
-    // std::cout << '\n';
-    
 
-    std::vector salaries_in_cents {
-            112323, 328318, 456756, 238954, 
-            545668, 452376, 456786, 908990,
-            581745, 475153, 149551
+    std::ranges::for_each(names, []( auto item) { std::cout<< item << ' '; });
+    endline();   
+   
+
+    std::vector<int>  salaries_in_cents {
+            1123'23, 3283'18, 4567'56, 2389'54, 
+            5456'68, 4523'76, 4567'86, 9089'90,
+            5817'45, 4751'53, 1495'51
         };
-
-        
-
-    // for (auto salary : salaries_in_cents) std::cout << salary << ' ';
-    // std::cout << '\n';
-    
-
-    std::transform(std::begin(salaries_in_cents), std::end(salaries_in_cents), std::begin(salaries_in_cents),
-            [](auto salary) { return salary * 1.35;});
-
-    // auto salaries_sum = 
-    //     std::accumulate(std::begin(salaries_in_cents), 
-    //                     std::end(salaries_in_cents), 
-    //                     0,  std::plus{});
-
-    std::cout << '\n';
-    // std::cout << salaries_sum;
-    std::cout << '\n';
-
-
-    std::map<std::string, int> employees{};
-
-    for( std::string name : names) {
-        
-        std::back_inserter(employees, std::pair<std::string, int>{name, 0});
-        
-    }
-
-    for(auto [name, salary] : employees) std::cout << name << " - " << salary << '\n' ;
-
-    
 }
