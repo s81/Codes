b5a6a7db055ade4271fb32291b37039c1eeb1720
Samer William Al-Haddadin <samer.w.alhaddadin@gmail.com>
2020-10-27 23:19:34 +0300

    Changing Fonts; C-style Arry instead of Vector..

--- hello_in_ranges.cpp
+++ hello_in_ranges.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 
-auto endline = []() { std::cout << '\n'; };
+auto endline = []() -> { std::cout << '\n'; }; //== !=
 
 int main()
 {
-    std::vector<std::string> names{
+    std::string names[] = {
         "Majeda", "Samer", "Shatha", "Adham", "Sahem",
         "Fayek", "Lamar", "Essa", "Miral",
         "Natasha", "William"};
@@ -14,7 +13,11 @@ int main()
     std::ranges::for_each(names, [](auto item) { std::cout << item << ' '; });
     endline();
 
-    std::vector<int> salaries_in_cents{
+    std::ranges::sort(names);
+    std::ranges::for_each(names, [](auto item) { std::cout << item << ' '; });
+    endline();
+
+    int salaries_in_cents[] = {
         1123'23, 3283'18, 4567'56, 2389'54,
         5456'68, 4523'76, 4567'86, 9089'90,
         5817'45, 4751'53, 1495'51};
