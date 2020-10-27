9746b914cfc4a10f913532471705e371bd31a5cf
Samer William Al-Haddadin <samer.w.alhaddadin@gmail.com>
2020-10-27 21:22:55 +0300

    updating .. ?

--- hello_in_ranges.cpp
+++ hello_in_ranges.cpp
@@ -1,27 +1,25 @@
 #include <iostream>
 #include <vector>
-#include <map>
-#include <algorithm>
-#include <numeric>
 
-auto endline = []() { std::cout << '\n'; };
-
+auto endline = []() -> void { std::cout << '\n'; };
 
 int main()
 {
-    std::vector<std::string> names { 
-        "Majeda", "Samer", "Shatha", "Adham", "Sahem", 
-        "Fayek", "Lamar", "Essa", "Miral", 
-        "Natasha", "William"
-    };   
+    std::vector<std::string> names{
+        "Majeda", "Samer", "Shatha",
+        "Adham", "Sahem", "Fayek",
+        "Lamar", "Essa", "Miral",
+        "Natasha", "William"};
 
-    std::ranges::for_each(names, []( auto item) { std::cout<< item << ' '; });
-    endline();   
-   
+    std::ranges::for_each(names, [](auto item) { std::cout << item << ' '; });
+    endline();
 
-    std::vector<int>  salaries_in_cents {
-            1123'23, 3283'18, 4567'56, 2389'54, 
-            5456'68, 4523'76, 4567'86, 9089'90,
-            5817'45, 4751'53, 1495'51
-        };
+    std::vector<uint_fast64_t> salaries_in_cents{
+        1123'23, 3283'18, 4567'56,
+        2389'54, 5456'68, 4523'76,
+        4567'86, 9089'90, 5817'45,
+        4751'53, 1495'51};
+
+    std::ranges::for_each(salaries_in_cents, [](auto item) { std::cout << std:: << item << ' '; });
+    endline();
 }
