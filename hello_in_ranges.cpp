e874d1a13af24cb7ac4f3aebebd27e5c4d33da41
Samer William Al-Haddadin <samer.w.alhaddadin@gmail.com>
2020-10-27 22:23:01 +0300

    Latest Work: Ranges::for_each

--- hello_in_ranges.cpp
+++ hello_in_ranges.cpp
@@ -1,37 +1,23 @@
 #include <iostream>
+#include <vector>
+#include <map>
 #include <algorithm>
-#include <ranges>
+#include <numeric>
+
+auto endline = []() { std::cout << '\n'; };
 
 int main()
 {
-    std::string names[] =
-        {
-            "Majeda", "Samer", "Shatha", "Adham", "Sahem",
-            "Fayek", "Lamar", "Essa", "Miral",
-            "Natasha", "William"};
+    std::vector<std::string> names{
+        "Majeda", "Samer", "Shatha", "Adham", "Sahem",
+        "Fayek", "Lamar", "Essa", "Miral",
+        "Natasha", "William"};
 
-    for (auto name : names)
-        std::cout << name << ' ';
-    std::cout << '\n';
-    std::cout << '\n';
+    std::ranges::for_each(names, [](auto item) { std::cout << item << ' '; });
+    endline();
 
-    double salaries[] =
-        {
-            1123.23, 3283.18, 4567.56, 2389.54,
-            5456.68, 4523.76, 4567.86, 9089.90,
-            5817.45, 4751.53, 1495.51};
-
-    for (auto salary : salaries)
-        std::cout << salary << ' ';
-    std::cout << '\n';
-    std::cout << '\n';
-
-    auto updated_salaries = salaries | std::views::transform([](auto salary) { return salary * 1.35; });
-
-    auto sum = salaries | std::ranges::
-
-                                  for (auto new_salary : updated_salaries) std::cout
-                              << new_salary << ' ';
-    std::cout << '\n';
-    std::cout << '\n';
+    std::vector<int> salaries_in_cents{
+        1123'23, 3283'18, 4567'56, 2389'54,
+        5456'68, 4523'76, 4567'86, 9089'90,
+        5817'45, 4751'53, 1495'51};
 }
