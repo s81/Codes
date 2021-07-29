5030413b3006d24ab2c58c02aa1b234ec192130c
Samer William Khalaf <2960688+s81@users.noreply.github.com>
2021-07-29 13:36:19 +0300

    Remove Names

--- hello_in_ranges.cpp
+++ hello_in_ranges.cpp
@@ -5,10 +5,7 @@ auto endline = []() -> void { std::cout << '\n'; }; //== !=
 
 int main()
 {
-    std::string names[] = {
-        "Majeda", "Samer", "Shatha", "Adham", "Sahem",
-        "Fayek", "Lamar", "Essa", "Miral",
-        "Natasha", "William"};
+    std::string names[] = {...};
 
     std::ranges::for_each(names, [](auto item) { std::cout << item << ' '; });
     endline();
@@ -17,8 +14,5 @@ int main()
     std::ranges::for_each(names, [](auto item) { std::cout << item << ' '; });
     endline();
 
-    int salaries_in_cents[] = {
-        1123'23, 3283'18, 4567'56, 2389'54,
-        5456'68, 4523'76, 4567'86, 9089'90,
-        5817'45, 4751'53, 1495'51};
+    int salaries_in_cents[] = {...};
 }
