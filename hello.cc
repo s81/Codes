07453f855db6127301bf9c0d31d9a7ff72e61ff4
Samer William Khalaf <2960688+s81@users.noreply.github.com>
2022-10-22 23:34:43 +0300

    Review

--- hello.cc
+++ hello.cc
@@ -1,8 +1,16 @@
+
 // auto natural_numbers = std::views::iota(1);
+
 // numbers | std::views::take(10)
+
 // auto multiple_by_two = [] (int number) {return number * 2;};
+
 // auto even_numbers = natural_numbers | std::views::transform(multiple_by_two);
+
 // std::accumulate(std::begin(salaries_in_cents), std::end(salaries_in_cents), 0,  std::plus{});
+
 // std::map<std::string, int> employees{};
+
 // for( std::string name : names) employees[name] = 0;
+
 // for(auto [name, salary] : employees) std::cout << name << " - " << salary << '\n' ;
